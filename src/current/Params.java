0fee2ae44d62f73ba1689af9ddc3b7c8e7444a09
Louis-max <louis-max.harter@protonmail.com>
2026-01-20 16:11:41 +0100

    Backup

--- src/current/Params.java
+++ src/current/Params.java
@@ -4,19 +4,19 @@ import current.Robots.Robot;
 
 public class Params {
 
-    // Aggressivity, depending on map and phase of the game
-    public static int PARAMS_aggressivitySmall1 = 50; // [1, 100]
-    public static int PARAMS_aggressivitySmall2 = 70; // [1, 100]
-    public static int PARAMS_aggressivitySmall3 = 85; // [1, 100]
-    public static int PARAMS_aggressivitySmall4 = 95; // [1, 100]
-    public static int PARAMS_aggressivityMedium1 = 40; // [1, 100]
-    public static int PARAMS_aggressivityMedium2 = 60; // [1, 100]
-    public static int PARAMS_aggressivityMedium3 = 80; // [1, 100]
-    public static int PARAMS_aggressivityMedium4 = 90; // [1, 100]
-    public static int PARAMS_aggressivityLarge1 = 30; // [1, 100]
-    public static int PARAMS_aggressivityLarge2 = 50; // [1, 100]
-    public static int PARAMS_aggressivityLarge3 = 70; // [1, 100]
-    public static int PARAMS_aggressivityLarge4 = 85; // [1, 100]
+    // Aggressivity, depending on MAP_ and phase of the game
+    public static int PARAMS_aggressivityMAP_SMALL1 = 50; // [1, 100]
+    public static int PARAMS_aggressivityMAP_SMALL2 = 70; // [1, 100]
+    public static int PARAMS_aggressivityMAP_SMALL3 = 85; // [1, 100]
+    public static int PARAMS_aggressivityMAP_SMALL4 = 95; // [1, 100]
+    public static int PARAMS_aggressivityMAP_MEDIUM1 = 40; // [1, 100]
+    public static int PARAMS_aggressivityMAP_MEDIUM2 = 60; // [1, 100]
+    public static int PARAMS_aggressivityMAP_MEDIUM3 = 80; // [1, 100]
+    public static int PARAMS_aggressivityMAP_MEDIUM4 = 90; // [1, 100]
+    public static int PARAMS_aggressivityMAP_LARGE1 = 30; // [1, 100]
+    public static int PARAMS_aggressivityMAP_LARGE2 = 50; // [1, 100]
+    public static int PARAMS_aggressivityMAP_LARGE3 = 70; // [1, 100]
+    public static int PARAMS_aggressivityMAP_LARGE4 = 85; // [1, 100]
     public static int[] aggresivity;
 
     public static int PARAMS_DANGER_OUT_OF_ENEMY_VIEW      = -3; // [-30, -1]
@@ -29,10 +29,10 @@ public class Params {
 
     public static void init(){
         aggresivity = switch (Robot.mapType){
-            case 0 -> new int[]{PARAMS_aggressivitySmall1,PARAMS_aggressivitySmall2,PARAMS_aggressivitySmall3,PARAMS_aggressivitySmall4};
-            case 1 -> new int[]{PARAMS_aggressivityMedium1,PARAMS_aggressivityMedium2,PARAMS_aggressivityMedium3,PARAMS_aggressivityMedium4};
-            case 2 -> new int[]{PARAMS_aggressivityLarge1,PARAMS_aggressivityLarge2,PARAMS_aggressivityLarge3,PARAMS_aggressivityLarge4};
-            default -> throw new IllegalStateException("Unexpected value: Maptype " + Robot.mapType);
+            case 0 -> new int[]{PARAMS_aggressivityMAP_SMALL1,PARAMS_aggressivityMAP_SMALL2,PARAMS_aggressivityMAP_SMALL3,PARAMS_aggressivityMAP_SMALL4};
+            case 1 -> new int[]{PARAMS_aggressivityMAP_MEDIUM1,PARAMS_aggressivityMAP_MEDIUM2,PARAMS_aggressivityMAP_MEDIUM3,PARAMS_aggressivityMAP_MEDIUM4};
+            case 2 -> new int[]{PARAMS_aggressivityMAP_LARGE1,PARAMS_aggressivityMAP_LARGE2,PARAMS_aggressivityMAP_LARGE3,PARAMS_aggressivityMAP_LARGE4};
+            default -> throw new IllegalStateException("Unexpected value: MAP_type " + Robot.mapType);
         };
     }
 }
---
+++ src/testMapSize/RobotPlayer.java
@@ -0,0 +1,28 @@
+package testMapSize;
+
+import battlecode.common.*;
+
+public class RobotPlayer {
+    public static void run(RobotController rc) throws GameActionException {
+        int mapType = 0; // Small
+        if(rc.getMapWidth() * rc.getMapHeight() > 32*32){
+            mapType = 1; // Medium
+        }
+        if(rc.getMapWidth() * rc.getMapHeight() > 45*45){
+            mapType = 2; // Big
+        }
+
+        switch(mapType){
+            case 1:
+                System.out.println("MAPTYPE: MAP_SMALL");
+                break;
+            case 2:
+                System.out.println("MAPTYPE: MAP_MEDIUM");
+                break;
+            case 3:
+                System.out.println("MAPTYPE: MAP_LARGE");
+                break;
+        }
+        rc.resign();
+    }
+}
